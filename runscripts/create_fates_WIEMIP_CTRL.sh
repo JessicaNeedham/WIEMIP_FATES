b77e1de7aab2376d31ba3004164313459cd320db
Jessica Needham <jessica@uan02.head.cm.americas.sgi.com>
2026-05-18 20:11:10 +0200

    add runscripts for BGC, CTRL and COU

--- runscripts/create_fates_WIEMIP_CTRL.sh
+++ runscripts/create_fates_WIEMIP_CTRL.sh
@@ -7,7 +7,7 @@ export PROJECT='nn9188k'
 export USER='jessica'
 export workpath='/cluster/work/projects/nn9188k/jessica'
 
-export TAG='noresm-fates-f09-wiemip-postAD-spinup'
+export TAG='noresm-fates-f09-wiemip-CTRL'
 export CASEROOT=$workpath/wiemip_runs
 export CIMEROOT=$workpath/noresm-wiemip-beta16/CTSM/cime/scripts
 
@@ -28,23 +28,24 @@ rm -rf ${CASE_NAME}
 
 cd ${CASE_NAME}
 
+# 
 ./xmlchange STOP_N=25
 ./xmlchange STOP_OPTION=nyears
 ./xmlchange REST_N=25
 ./xmlchange REST_OPTION=nyears
-./xmlchange RESUBMIT=3
+./xmlchange RESUBMIT=5
 ./xmlchange DEBUG=FALSE
 
-./xmlchange RUN_STARTDATE=0401-01-01 # check this matches end of AD spinup run
+./xmlchange RUN_STARTDATE=1850-01-01 # check this matches end of AD spinup run
 ./xmlchange CLM_ACCELERATED_SPINUP=off
 ./xmlchange CCSM_CO2_PPMV=280.
 ./xmlchange DATM_YR_START=1850
 ./xmlchange DATM_YR_END=1869
 ./xmlchange DATM_PRESAERO=clim_1850
 
+# For real runs
 ./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=24:00:00
 ./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=00:30:00
-
 
 ./xmlchange RUNDIR=${CASE_NAME}/run
 #./xmlchange EXEROOT=${CASE_NAME}/bld
@@ -55,27 +56,24 @@ cd ${CASE_NAME}
 
 cat >>  user_nl_clm <<EOF
 finidat=''
-fsurdat='/cluster/work/projects/nn9560k/inputdata/lnd/clm2/surfdata_esmf/ctsm5.4.0/surfdata_0.9x1.25_hist_1850_16pfts_WIEMIP_c260408.nc'
 use_fates_sp=.false.
 use_fates_nocomp=.true.
 use_fates_fixed_biogeog=.true.
 do_transient_lakes = .false.
 do_transient_urban = .false.
 fates_stomatal_model='medlyn2011'
-fates_spitfire_mode=4
-stream_year_first_popdens=1850
-stream_year_last_popdens=1850
-model_year_align_popdens=1850
-fates_lu_transition_logic = 1
 use_fates_luh=.true.
 use_fates_lupft=.true.
 fates_harvest_mode='luhdata_area'
 use_fates_potentialveg=.false.
 fluh_timeseries='/cluster/work/projects/nn9188k/jessica/wiemip-lufiles/LUH2_1850_steadystate_0.9x1.25_c260515.nc'
 flandusepftdat='/cluster/work/projects/nn9188k/jessica/wiemip-lufiles/fates_landuse_pft_surfdata_0.9x1.25_c260515.nc'
+fates_spitfire_mode=4
 hist_empty_htapes=.true.
-hist_fincl1='FCO2', 'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU',
-'FATES_VEGC_PF', 'FATES_VEGC_LU', 'FATES_LAI', 'FATES_GPP_PF'
+hist_fincl1='FCO2', 'FATES_GPP_LU', 
+'FATES_VEGC_LU', 'FATES_PATCHAREA_LU', 'FATES_NPP_LU', 'FATES_LUCHANGE_WOODPROD_C_FLUX', 'FATES_DISTURBANCE_RATE_LOGGING',
+'FATES_VEGC_ABOVEGROUND', 'FATES_VEGC', 'FATES_FRACTION', 'FATES_GPP','FATES_NEP','FATES_AUTORESP', 'FATES_HET_RESP', 'QVEGE',
+ 'QVEGT','QSOIL','EFLX_LH_TOT','FSH','FSR', 'FSDS','FSA','FIRE','FLDS','FATES_LAI', 'FATES_VEGC_PF'
 EOF
 
 cp /cluster/work/projects/nn9560k/inputdata/WIEMIP/wiemip_spinup_datm.streams user_nl_datm_streams
