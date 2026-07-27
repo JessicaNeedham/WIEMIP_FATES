8b4258a36acdf4ca1815c0a61fe0723636ee4438
Jessica Needham <jessica@uan02.head.cm.americas.sgi.com>
2026-07-27 15:20:20 +0200

    add future scenario run scripts

--- runscripts/create_fates_WIEMIP_future_postAD_spinup.sh
+++ runscripts/create_fates_WIEMIP_future_postAD_spinup.sh
@@ -4,11 +4,10 @@ export COMPSET='1850_DATM%CRUJRA2024_CLM60%FATES_SICE_SOCN_SROF_SGLC_SWAV_SESP'
 export RES=f09_g17
 export MACH='olivia'
 export PROJECT='nn9188k'
-#export COMPILER=gnu
 export USER='jessica'
 export workpath='/cluster/work/projects/nn9188k/jessica'
 
-export TAG='noresm-fates-f09-wiemip-future_AD-spinup'
+export TAG='noresm-fates-f09-wiemip-future-postAD-spinup'
 export CASEROOT=$workpath/wiemip_runs
 export CIMEROOT=$workpath/noresm-wiemip-future/CTSM/cime/scripts
 
@@ -16,7 +15,6 @@ cd ${CIMEROOT}
 
 export CIME_HASH=`git log -n 1 --pretty=%h`
 export NorESM_CTSM_HASH=`(cd ../..;git log -n 1 --pretty=%h)`
-echo $PWD
 export FATES_HASH=`(cd src/fates;git log -n 1 --pretty=%h)`
 export GIT_HASH=N${NorESM_CTSM_HASH}-F${FATES_HASH}	
 export CASE_NAME=${CASEROOT}/${TAG}.`date +"%Y-%m-%d"`
@@ -37,21 +35,17 @@ cd ${CASE_NAME}
 ./xmlchange RESUBMIT=15
 ./xmlchange DEBUG=FALSE
 
-./xmlchange RUN_STARTDATE=0001-01-01
-./xmlchange CLM_ACCELERATED_SPINUP=on
-./xmlchange CLM_FORCE_COLDSTART=on
+./xmlchange RUN_STARTDATE=0401-01-01 # check this matches end of AD spinup run
+./xmlchange CLM_ACCELERATED_SPINUP=off
 ./xmlchange CCSM_CO2_PPMV=287.43
 ./xmlchange DATM_YR_START=1850
 ./xmlchange DATM_YR_END=1869
-./xmlchange DATM_YR_ALIGN=1
-
 ./xmlchange DATM_PRESAERO=clim_1850
 
 ./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=24:00:00
 ./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=00:30:00
- 
-./xmlchange RUNDIR=${CASE_NAME}/run
-./xmlchange EXEROOT=${CASE_NAME}/bld
+
+./xmlchange CLM_BLDNML_OPTS="-bgc fates -megan"
 
 ./xmlchange NTASKS_CPL=1536
 ./xmlchange NTASKS_ATM=256
@@ -60,13 +54,17 @@ cd ${CASE_NAME}
 ./xmlchange ROOTPE_ATM=0
 ./xmlchange ROOTPE_LND=256
 
+./xmlchange RUNDIR=${CASE_NAME}/run
+#./xmlchange EXEROOT=${CASE_NAME}/bld
 
-# turn on BVOCs
-./xmlchange CLM_BLDNML_OPTS="-bgc fates -megan"
+# use existing build
+./xmlchange BUILD_COMPLETE=TRUE
+./xmlchange EXEROOT=/cluster/work/projects/nn9188k/jessica/wiemip_runs/noresm-fates-f09-wiemip-future_AD-spinup.2026-06-24/bld
 
 cat >>  user_nl_clm <<EOF
+fates_paramfile='/cluster/home/jessica/WIEMIP/paramfiles/fates_params_noresm_wiemip.json'
+finidat='/cluster/work/projects/nn9188k/jessica/wiemip_runs/noresm-fates-f09-wiemip-future_AD-spinup.2026-06-24/run/noresm-fates-f09-wiemip-future_AD-spinup.2026-06-24.clm2.r.0401-01-01-00000.nc'
 fsurdat='/cluster/work/projects/nn9560k/inputdata/lnd/clm2/surfdata_esmf/ctsm5.4.0/surfdata_0.9x1.25_hist_1850_16pfts_WIEMIP_c260408.nc'
-fates_paramfile = '/cluster/work/projects/nn9188k/jessica/noresm-wiemip-beta16/CTSM/src/fates/parameter_files/fates_params_noresm.json'
 use_fates_sp=.false.
 use_fates_nocomp=.true.
 use_fates_fixed_biogeog=.true.
@@ -76,7 +74,6 @@ fates_stomatal_model='medlyn2011'
 fates_spitfire_mode=4
 stream_year_first_popdens=1850
 stream_year_last_popdens=1850
-model_year_align_popdens=1850
 fates_lu_transition_logic = 1
 use_fates_luh=.true.
 use_fates_lupft=.true.
@@ -85,13 +82,12 @@ use_fates_potentialveg=.false.
 fluh_timeseries='/cluster/work/projects/nn9188k/jessica/wiemip-lufiles/LUH2_2023_steadystate_0.9x1.25_c260622.nc'
 flandusepftdat='/cluster/work/projects/nn9188k/jessica/wiemip-lufiles/fates_landuse_pft_surfdata_0.9x1.25_c260515.nc'
 hist_empty_htapes=.true.
-hist_fincl1='FCO2','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU','FATES_GPP_LU',
-'FATES_VEGC_PF', 'FATES_VEGC_LU', 'FATES_LAI', 'FATES_GPP_PF'
+hist_fincl1='FCO2', 'FATES_SECONDARY_AREA_ANTHRO_AP','FATES_SECONDARY_AREA_AP','FATES_PRIMARY_AREA_AP','FATES_NPP_LU',
+'FATES_GPP_LU','FATES_VEGC_PF', 'FATES_VEGC_LU', 'FATES_LAI', 'FATES_GPP_PF'
 EOF
 
 cp /cluster/work/projects/nn9560k/inputdata/WIEMIP/wiemip_future_spinup_datm.streams user_nl_datm_streams
 
 ./case.setup
-./case.build
+#./case.build
 ./case.submit
-
